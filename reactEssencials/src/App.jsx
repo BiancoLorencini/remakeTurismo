00bc68f7e210244999966204b4bdbcea5d8d86bb
Bianco <arthnilo@gmail.com>
2024-08-17 10:28:22 -0300

    feat: react-spring & renderização condicional no expanded

--- reactEssencials/src/App.jsx
+++ reactEssencials/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, createElement } from 'react'
+import { useSpring, animated } from '@react-spring/web'
 import style from './App.module.css'
 import videoBackground from './assets/vids/drone.mp4'
 import tereFoto from './assets/teresópolis/pedraDoSino.jpg'
@@ -12,6 +13,15 @@ function App() {
   const containerRef = useRef(null)
   const [scrollPosition, setScrollPosition] = useState('top');
   const [stickyItem, setStickyItem] = useState(false);
+
+  const [props] = useSpring(
+    () => ({
+      from: { opacity: 0 , display: 'none'}, 
+      to: { opacity: 1 , display: 'block'},
+      delay: 4000,
+    }),
+    []
+  )
   const handleMouseOver = (index) => {
     setActiveIndex(index);
   }
@@ -26,14 +36,30 @@ function App() {
 
     setStickyItem(!stickyItem);
 
+    if (!isExpanded) {
+      requestAnimationFrame(() => {
+          containerRef.current.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start',
+          });
+      });
+  }
+
     requestAnimationFrame(() => {
       containerRef.current.scrollIntoView({
         behavior: 'smooth',
         block: 'end',
-      });
+      })
     });
   }
 
+  useEffect(() => {
+    if (isExpanded === true) {
+      createElement('div', {className: style.infoCard}, null);
+    }
+  }, [isExpanded]);
+
+  
   useEffect(() => {
     if (scrollPosition === 'top') {
       containerRef.current.scrollIntoView({
@@ -48,22 +74,20 @@ function App() {
     }
   }, [isExpanded, scrollPosition]);
 
-
-
   return (
     <>
       <div className={`${style.mainContainer} ${
             isExpanded === true ? style.mainContainerExpanded : ''
           }`} >
         <video autoPlay muted loop className={style.video}>
-            <source src={videoBackground} type="video/mp4" />
-            Seu navegador não suporta vídeos em HTML5.
+          <source src={videoBackground} type="video/mp4"/>
+          Seu navegador não suporta vídeos em HTML5.
         </video>
-        <nav className={style.nav} >
-          <ul className={style.navList} >
-            <li className={style.navItem}>Home</li>
-            <li className={style.navItem}>Sobre</li>
-            <li className={style.navItem}>Contato</li>
+        <nav className={style.nav}>
+          <ul className={style.navList} > 
+            <animated.div style={props}> <li className={style.navItem}>Home</li></animated.div>
+            <animated.div style={props}><li className={style.navItem}>Sobre</li></animated.div>
+            <animated.div style={props}><li className={style.navItem}>Contato</li></animated.div>
           </ul>
         </nav>
         <section className={style.container01}>
@@ -72,33 +96,38 @@ function App() {
           }`} ref={containerRef} id='container02' >
             <img onClick={() => setActiveIndex(null)} className={style.logo} src={logo} alt="BioGroup Logo" />
             <p>© 2024 - Todos os direitos reservados</p>
+            {isExpanded && (
+              <div className={style.expandedInfoCard}>
+                <p>Informações adicionais ao expandir.</p>
+              </div>
+            )}
           </section>
           <section className={style.container03}>
-              <ul className={style.lista}>
-                <li
-                  onClick={() => handleNavClick(1)}
-                  onMouseOver={() => handleMouseOver(1)} 
-                  onMouseOut={handleMouseOut}
-                  className={`${style.itemLista} ${style.teresopolis} ${activeIndex !== null && activeIndex !== 1 ? style.inactive : ''} ${stickyItem ? style.stickyFotoTeresopolis : ''}`}>
-                  <img className={style.listaImagem} src={tereFoto} alt="foto alto da pedra do sino" />
-                  <p>Teresópolis</p>
-                </li>
-                <li 
-                  onMouseOver={() => handleMouseOver(2)} 
-                  onMouseOut={handleMouseOut}
-                  className={`${style.itemLista} ${style.friburgo} ${activeIndex !== null && activeIndex !== 2 ? style.inactive : ''}`}>
-                  <img className={style.listaImagem} src={friFoto} alt="foto do country clube de Friburgo" />
-                  <p>Friburgo</p>
-                </li>
-                <li 
-                  onMouseOver={() => handleMouseOver(3)} 
-                  onMouseOut={handleMouseOut}
-                  className={`${style.itemLista} ${style.petropolis} ${activeIndex !== null && activeIndex !== 3 ? style.inactive : ''}`}>
-                  <img className={style.listaImagem} src={petFoto} alt="foto do palácio de Petrópolis" />
-                  <p>Petrópolis</p>
-                </li>
-              </ul>
-            </section>
+            <ul className={style.lista}>
+              <li
+                onClick={() => handleNavClick(1)}
+                onMouseOver={() => handleMouseOver(1)} 
+                onMouseOut={handleMouseOut}
+                className={`${style.itemLista} ${style.teresopolis} ${activeIndex !== null && activeIndex !== 1 ? style.inactive : ''} ${stickyItem ? style.stickyFotoTeresopolis : ''}`}>
+                <img className={style.listaImagem} src={tereFoto} alt="foto alto da pedra do sino" />
+                <p>Teresópolis</p>
+              </li>
+              <li 
+                onMouseOver={() => handleMouseOver(2)} 
+                onMouseOut={handleMouseOut}
+                className={`${style.itemLista} ${style.friburgo} ${activeIndex !== null && activeIndex !== 2 ? style.inactive : ''}`}>
+                <img className={style.listaImagem} src={friFoto} alt="foto do country clube de Friburgo" />
+                <p>Friburgo</p>
+              </li>
+              <li 
+                onMouseOver={() => handleMouseOver(3)} 
+                onMouseOut={handleMouseOut}
+                className={`${style.itemLista} ${style.petropolis} ${activeIndex !== null && activeIndex !== 3 ? style.inactive : ''}`}>
+                <img className={style.listaImagem} src={petFoto} alt="foto do palácio de Petrópolis" />
+                <p>Petrópolis</p>
+              </li>
+            </ul>
+          </section>
         </section>
         <a className={style.referenciaVideo} href="https://pixabay.com/pt/users/elegancefariamodacrist-23526092/" target="_blank" rel="noopener noreferrer">Agradecimento <br/> elegancefariamodacrist no Pixabay</a>
       </div>
